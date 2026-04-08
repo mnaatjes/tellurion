d776822c22d3de4d62ed401f8390ac6939c6658a
mnaatjes <michael.naatjes87@gmail.com>
2026-04-08 14:09:28 +0000

    added tests/

---
+++ tests/unit/__test_prototype.py
@@ -0,0 +1,68 @@
+# tests/unit/test_prototype.py
+import pytest
+from rich import inspect
+from pathlib import Path
+from llama_index.core import SimpleDirectoryReader, Settings, VectorStoreIndex, StorageContext
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.extractors import TitleExtractor, SummaryExtractor
+from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
+
+def test_run():
+    # Configure
+    embedding = GoogleGenAIEmbedding(
+        model_name="models/gemini-embedding-2-preview",
+        api_key="",
+        embed_batch_size=25,
+        retries=5,
+        timeout=30,
+        num_workers=4,
+        task_type="retrieval_document"
+    )
+    
+    #llm = GoogleGenAI(model="models/gemini-2.5-flash", api_key="")
+    llm = GoogleGenAI()
+    Settings.embed_model = embedding
+    Settings.llm = llm
+
+    LLAMA_CLONE = Path("tests/data/sources/tmp/llama_index_clone")
+    LLAMA_SINK  = Path("tests/data/llama_index/")
+    target_dir = "docs/src/content/docs/framework/"
+    input_dir = Path(LLAMA_CLONE / target_dir)
+    output_dir = Path(LLAMA_SINK / target_dir)
+
+    # Assign Reader
+    reader = SimpleDirectoryReader(input_dir=str(input_dir))
+    
+    # Read Content
+    documents = reader.load_data()
+    
+    # Parse
+    #Initialize Parser
+    parser = SentenceSplitter(chunk_size=512, chunk_overlap=20)
+    nodes = parser.get_nodes_from_documents(documents)
+
+    # Enrichment
+    # Define Strategy
+    extractors = [
+        TitleExtractor(nodes=5),
+        SummaryExtractor(summaries=["self","prev"])
+    ]
+
+    # Loop Extractor Strategies
+    for ext in extractors:
+        # Execute API Call
+        metadata_dicts = ext.extract(nodes)
+
+        # Zip information from dict into node
+        for node, metadata in zip(nodes, metadata_dicts):
+            node.metadata.update(metadata)
+
+    # Embed and Index
+    index = VectorStoreIndex(nodes)
+
+    # Persist
+    index.storage_context.persist(persist_dir=str(output_dir))
+
+    print("LLAMA INDEX Vector Store COMPLETE!")
